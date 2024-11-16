286b38d07440715d8ceef24b01ba8bbffa07943d
Bilalabdali1 <73677648+Bilalabdali1@users.noreply.github.com>
2024-11-16 16:50:17 -0500

    Time: 0 ms (100.00%) | Memory: 11.6 MB (28.09%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        leftPointer=0
+        for rightPointer in range(len(nums)):
+            if nums[rightPointer]!=val:
+                nums[leftPointer]=nums[rightPointer]
+                leftPointer+=1
+        return leftPointer

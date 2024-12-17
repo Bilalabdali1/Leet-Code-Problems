de853e5a964e836d7608d17cb1a0e219958cdd66
Bilalabdali1 <73677648+Bilalabdali1@users.noreply.github.com>
2024-12-17 13:20:11 -0500

    Time: 0 ms (100.00%) | Memory: 13.9 MB (10.50%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -9,5 +9,4 @@ class Solution(object):
             if nums[rightPointer]!=nums[rightPointer-1]:
                 nums[leftPointer]=nums[rightPointer]
                 leftPointer+=1
-        return leftPointer 
-        
+        return leftPointer
